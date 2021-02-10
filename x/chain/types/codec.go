c0eb81eca3826ae275d5a36c4d4667f025ffd0c0
Segfault <tyldar@users.noreply.github.com>
2021-02-10 14:47:43 +0100

    missing message register

--- x/chain/types/codec.go
+++ x/chain/types/codec.go
@@ -11,11 +11,15 @@ import (
 // RegisterCodec Register the codec for  the message passing
 func RegisterCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgOpenBeam{}, "sandblock/OpenBeam", nil)
+	cdc.RegisterConcrete(&MsgIncreaseBeam{}, "sandblock/IncreaseBeam", nil)
+	cdc.RegisterConcrete(&MsgClaimBeam{}, "sandblock/ClaimBeam", nil)
+	cdc.RegisterConcrete(&MsgCancelBeam{}, "sandblock/CancelBeam", nil)
+	cdc.RegisterConcrete(&MsgCloseBeam{}, "sandblock/CloseBeam", nil)
 }
 
 // RegisterInterfaces Register the implementations for the given codecs
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgOpenBeam{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgOpenBeam{}, &MsgIncreaseBeam{}, &MsgClaimBeam{}, &MsgCancelBeam{}, &MsgCloseBeam{})
 }
 
 var (
