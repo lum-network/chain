48434fe68f40a5d1d9d6ada12ae49da2c70d4d85
Segfault <5221072+Segfaultd@users.noreply.github.com>
2021-09-28 12:37:41 +0200

    Backported a few REST query endpoints for legacy integration

--- x/beam/client/rest/rest.go
+++ x/beam/client/rest/rest.go
@@ -13,7 +13,17 @@ const (
 
 // RegisterRoutes registers chain-related REST handlers to a router
 func RegisterRoutes(clientCtx client.Context, r *mux.Router) {
-    // this line is used by starport scaffolding # 2
+	r.HandleFunc("/bank/balances/{address}", QueryBalancesRequestHandlerFn(clientCtx)).Methods("GET")
+	r.HandleFunc("/supply/total", totalSupplyHandlerFn(clientCtx)).Methods("GET")
+	r.HandleFunc("/supply/total/{denom}", supplyOfHandlerFn(clientCtx)).Methods("GET")
+
+	r.HandleFunc("/staking/delegators/{delegatorAddr}/delegations", delegatorDelegationsHandlerFn(clientCtx)).Methods("GET")
+	r.HandleFunc("/staking/delegators/{delegatorAddr}/unbonding_delegations", delegatorUnbondingDelegationsHandlerFn(clientCtx), ).Methods("GET")
+	r.HandleFunc("/staking/pool", poolHandlerFn(clientCtx), ).Methods("GET")
+
+	r.HandleFunc("/distribution/delegators/{delegatorAddr}/rewards", delegatorRewardsHandlerFn(clientCtx)).Methods("GET")
+
+	r.HandleFunc("/minting/inflation", queryInflationHandlerFn(clientCtx)).Methods("GET")
 }
 
 func registerQueryRoutes(clientCtx client.Context, r *mux.Router) {
