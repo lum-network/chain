e4960b66f7e8911348d91f89043b56b9beb84760
Segfault <5221072+Segfaultd@users.noreply.github.com>
2022-03-11 17:24:57 +0100

    Added another script to allow for duplicate IP

--- scripts/apply_default_config.py
+++ scripts/apply_default_config.py
@@ -1,17 +1,13 @@
 import logging
-import argparse
 import socket
+import argparse
 import os
 import toml
 
-def apply_params(home, servers, height, hash):
+def apply_params(home):
     path = "{}/config/config.toml".format(home)
     data = toml.load(path)
-    data['statesync']['enable'] = True
-    data['statesync']['rpc_servers'] = servers
-    data['statesync']['trust_height'] = height
-    data['statesync']['trust_hash'] = hash
-    data['statesync']['trust_period'] = '336h'
+    data['p2p']['allow_duplicate_ip'] = True
 
     f = open(path, 'w')
     toml.dump(data, f)
@@ -24,12 +20,9 @@ def main():
     # Setup the arguments
     parser = argparse.ArgumentParser(description='Apply state sync parameters to config.toml file')
     parser.add_argument('--home', type=str, help='The path to the chain root folder', required=True)
-    parser.add_argument('--servers', type=str, help='The snapshot servers to use', required=True)
-    parser.add_argument('--height', type=str, help='The snapshot height', required=True)
-    parser.add_argument('--hash', type=str, help='The snapshot hash', required=True)
     args = parser.parse_args()
 
-    apply_params(args.home, args.servers, args.height, args.hash)
+    apply_params(args.home)
     logging.info("Applied params to config.toml file")
 
 if __name__ == "__main__":
