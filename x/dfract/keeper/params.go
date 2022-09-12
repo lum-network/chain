694e71a32761c68c82e5e17977fbc7de2c78e875
Segfault <5221072+Segfaultd@users.noreply.github.com>
2022-09-12 17:51:01 +0200

    Added default params

--- x/dfract/keeper/params.go
+++ x/dfract/keeper/params.go
@@ -23,3 +23,11 @@ func (k Keeper) SetParams(ctx sdk.Context, params types.Params) error {
 	store.Set([]byte(types.ParamsKey), bz)
 	return nil
 }
+
+// DefaultParams return the default dfract module params
+func DefaultParams() types.Params {
+	return types.Params{
+		DepositDenom: "ulum",
+		MintDenom:    "dfr",
+	}
+}
