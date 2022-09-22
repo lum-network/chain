356ee280f3887a82a4fb1987f5f691a50ca99c9f
Segfault <5221072+Segfaultd@users.noreply.github.com>
2022-09-22 14:26:59 +0200

    Register interface implementations

--- x/dfract/module.go
+++ x/dfract/module.go
@@ -49,7 +49,7 @@ func (AppModuleBasic) RegisterCodec(cdc *codec.LegacyAmino) {
 }
 
 func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
-	cdc.RegisterConcrete(&types.WithdrawAndMintProposal{}, "lum-network/WithdrawAndMintProposal", nil)
+	types.RegisterCodec(cdc)
 }
 
 // RegisterInterfaces registers the module's interface types
--- x/dfract/types/codec.go
+++ x/dfract/types/codec.go
@@ -3,16 +3,17 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
 // RegisterCodec Register the codec for  the message passing
 func RegisterCodec(cdc *codec.LegacyAmino) {
-
+	cdc.RegisterConcrete(&WithdrawAndMintProposal{}, "lum-network/WithdrawAndMintProposal", nil)
 }
 
 // RegisterInterfaces Register the implementations for the given codecs
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-
+	registry.RegisterImplementations((*govtypes.Content)(nil), &WithdrawAndMintProposal{})
 }
 
 var (
