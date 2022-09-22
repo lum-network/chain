070c02101b262871a83a98b61a0d16bce622ffb5
Segfault <5221072+Segfaultd@users.noreply.github.com>
2022-09-22 17:52:54 +0200

    Added missing codec implementation registry

--- x/dfract/types/codec.go
+++ x/dfract/types/codec.go
@@ -3,16 +3,19 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
 // RegisterCodec Register the codec for  the message passing
 func RegisterCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgDeposit{}, "lum-network/Deposit", nil)
 	cdc.RegisterConcrete(&WithdrawAndMintProposal{}, "lum-network/WithdrawAndMintProposal", nil)
 }
 
 // RegisterInterfaces Register the implementations for the given codecs
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgDeposit{})
 	registry.RegisterImplementations((*govtypes.Content)(nil), &WithdrawAndMintProposal{})
 }
 
