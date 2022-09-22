353f0f8b443743bc82c4145001ebec55b85022c8
Segfault <5221072+Segfaultd@users.noreply.github.com>
2022-09-22 21:21:06 +0200

    Register and seal

--- x/dfract/types/codec.go
+++ x/dfract/types/codec.go
@@ -2,7 +2,9 @@ package types
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
@@ -13,8 +15,8 @@ var (
 	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
 )
 
-// RegisterCodec Register the codec for  the message passing
-func RegisterCodec(cdc *codec.LegacyAmino) {
+// RegisterLegacyAminoCodec Register the codec for the message passing
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgDeposit{}, "lum-network/Deposit", nil)
 	cdc.RegisterConcrete(&WithdrawAndMintProposal{}, "lum-network/WithdrawAndMintProposal", nil)
 }
@@ -25,4 +27,13 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*govtypes.Content)(nil), &WithdrawAndMintProposal{})
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+}
+
+func init() {
+	RegisterLegacyAminoCodec(amino)
+	cryptocodec.RegisterCrypto(amino)
+	amino.Seal()
+
+	// Specific register call for Authz
+	RegisterLegacyAminoCodec(legacy.Cdc)
 }
