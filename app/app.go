f309edbb3b7149fa8166198192cc80a3404bf7f1
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-01-13 12:13:23 -0500

    Add v1.3.2 upgrade handler

--- app/app.go
+++ app/app.go
@@ -656,12 +656,16 @@ func (app *App) registerUpgradeHandlers() {
 	})
 
 	app.UpgradeKeeper.SetUpgradeHandler("v1.3.1", func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		// Set the IBCFee module consensus version so InitGenesis is not run
-		fromVM[ibcfeetypes.ModuleName] = app.mm.Modules[ibcfeetypes.ModuleName].ConsensusVersion()
-
 		// Apply the new dfract params map
 		app.DFractKeeper.SetParams(ctx, dfracttypes.DefaultParams())
 		app.Logger().Info("v1.3.1 upgrade applied")
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
+
+	app.UpgradeKeeper.SetUpgradeHandler("v1.3.2", func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		// Set the IBCFee module consensus version so InitGenesis is not run
+		fromVM[ibcfeetypes.ModuleName] = app.mm.Modules[ibcfeetypes.ModuleName].ConsensusVersion()
+		app.Logger().Info("v1.3.2 upgrade applied")
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	})
 
