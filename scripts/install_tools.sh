9c539c7c51347c8475f218b7fcbbd25a9a91ae43
Fabrice Bascoulergue <lebascou@users.noreply.github.com>
2023-06-01 17:03:55 +0200

    Fix workflow

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -2,4 +2,4 @@ echo "Installing golangci..."
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.52.2
 
 echo "Installing gosec..."
-curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b $(go env GOPATH)/bin vX.Y.Z
+curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.13.1
