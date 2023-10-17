26f5797de15c2c9f3354555591ff69a8ccc442b5
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-10-17 18:10:53 +0200

    Added queue pruning and other before-operation changes

--- x/beam/migrations/migrator.go
+++ x/beam/migrations/migrator.go
@@ -2,6 +2,7 @@ package migrations
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	v162 "github.com/lum-network/chain/x/beam/migrations/v162"
 
 	keeper2 "github.com/lum-network/chain/x/beam/keeper"
 	v110 "github.com/lum-network/chain/x/beam/migrations/v110"
@@ -18,4 +19,8 @@ func NewMigrator(keeper keeper2.Keeper) Migrator {
 // Migrate1To2 migrates from version 1 to 2
 func (m Migrator) Migrate1To2(ctx sdk.Context) error {
 	return v110.MigrateBeamQueues(ctx, m.keeper)
+}
+
+func (m Migrator) Migrate2To3(ctx sdk.Context) error {
+	return v162.DeleteBeamsData(ctx, m.keeper)
 }
