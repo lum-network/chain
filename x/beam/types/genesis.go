22be9b9108663ba8c7686c25f359233c7dcc0a91
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-10-20 11:30:27 +0200

    Remove genesis methods

--- x/beam/types/genesis.go
+++ x/beam/types/genesis.go
@@ -1,36 +1,11 @@
 package types
 
-import (
-	"encoding/json"
-
-	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-)
-
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
-	return &GenesisState{
-		Beams:                []*Beam{},
-		ModuleAccountBalance: sdk.NewCoin(DefaultBeamDenom, sdk.ZeroInt()),
-	}
-}
-
-// GetGenesisStateFromAppState returns x/beam GenesisState given raw application genesis state.
-func GetGenesisStateFromAppState(cdc codec.JSONCodec, appState map[string]json.RawMessage) *GenesisState {
-	var genesisState GenesisState
-
-	if appState[ModuleName] != nil {
-		cdc.MustUnmarshalJSON(appState[ModuleName], &genesisState)
-	}
-
-	return &genesisState
+	return nil
 }
 
 // Validate performs basic genesis state validation returning an error upon any failure.
 func (gs GenesisState) Validate() error {
-	if err := gs.ModuleAccountBalance.Validate(); err != nil {
-		return err
-	}
-
 	return nil
 }
--- x/dfract/types/genesis.go
+++ x/dfract/types/genesis.go
@@ -1,16 +1,5 @@
 package types
 
-func NewGenesisState(params Params) *GenesisState {
-	return &GenesisState{
-		Params: params,
-	}
-}
 func DefaultGenesisState() *GenesisState {
-	return &GenesisState{
-		Params: DefaultParams(),
-	}
-}
-
-func ValidateGenesis(data GenesisState) error {
 	return nil
 }
