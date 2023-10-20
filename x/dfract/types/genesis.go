43f1ad2eccd8864588b6a7924e8f06c3aebf74f8
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-10-20 11:34:19 +0200

    Revert "Remove genesis methods"
    This reverts commit 22be9b9108663ba8c7686c25f359233c7dcc0a91.

--- x/dfract/types/genesis.go
+++ x/dfract/types/genesis.go
@@ -1,5 +1,16 @@
 package types
 
+func NewGenesisState(params Params) *GenesisState {
+	return &GenesisState{
+		Params: params,
+	}
+}
 func DefaultGenesisState() *GenesisState {
+	return &GenesisState{
+		Params: DefaultParams(),
+	}
+}
+
+func ValidateGenesis(data GenesisState) error {
 	return nil
 }
