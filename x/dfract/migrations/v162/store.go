c38a3175fb927ae50ab7f2958cc6af08ca67a01b
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-10-20 11:45:25 +0200

    Only burn once

--- x/dfract/migrations/v162/store.go
+++ x/dfract/migrations/v162/store.go
@@ -32,12 +32,16 @@ func Nuke(ctx sdk.Context, dk dfractkeeper.Keeper) error {
 			if err := dk.BankKeeper.SendCoinsFromAccountToModule(ctx, account.GetAddress(), dfracttypes.ModuleName, sdk.NewCoins(supply)); err != nil {
 				panic(err)
 			}
-
-			// Burn the coins
-			if err := dk.BankKeeper.BurnCoins(ctx, dfracttypes.ModuleName, sdk.NewCoins(supply)); err != nil {
-				panic(err)
-			}
 		}
+	}
+
+	// Get the exact balance of the module account for the mint denom
+	moduleAccount := dk.AuthKeeper.GetModuleAccount(ctx, dfracttypes.ModuleName)
+	supply := dk.BankKeeper.GetBalance(ctx, moduleAccount.GetAddress(), dfracttypes.MintDenom)
+	
+	// Burn the coins
+	if err := dk.BankKeeper.BurnCoins(ctx, dfracttypes.ModuleName, sdk.NewCoins(supply)); err != nil {
+		panic(err)
 	}
 
 	return nil
